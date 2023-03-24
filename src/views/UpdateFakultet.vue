cc5ba18e1b76725cea16bd9045059e7ca825fb1c
Marija Jeremic <marija.jeremic@pioniri.com>
2023-03-24 16:09:33 +0100

    add validation for update faculty

--- src/views/UpdateFakultet.vue
+++ src/views/UpdateFakultet.vue
@@ -34,14 +34,17 @@
     <form action="" @submit.prevent="fakultetUpdate()">
      <div class="mb-2">
       <input v-model="fakultet.maticniBroj" type="text" class="form-control" placeholder="Matični Broj">
+      <span v-if="v$.fakultet.maticniBroj.$error">{{v$.fakultet.maticniBroj.$errors[0].$message}}</span>
      </div>
      <div class="mb-2">
       <input v-model="fakultet.naziv" type="text" class="form-control" placeholder="Naziv">
+      <span v-if="v$.fakultet.naziv.$error">{{v$.fakultet.naziv.$errors[0].$message}}</span>
      </div>
      <div class="mb-2">
       <select name="" id="" ref="selectMesto" v-model="fakultet.mestoId" class="form-control" v-if="mestoArr.length > 0">
          <option :value="mesto.ptt" v-for="mesto of mestoArr" :key="mesto.ptt">{{mesto.naziv}}</option>
       </select>
+      <span v-if="v$.fakultet.mestoId.$error">{{v$.fakultet.mestoId.$errors[0].$message}}</span>
       <!-- <input disabled :value="mesto.naziv" type="text" class="form-control"> -->
      </div>
      <div class="mt-3">
@@ -64,12 +67,17 @@
 import Spinner from '@/components/Spinner.vue'
 import {FakultetService} from '@/services/FakultetService'
 import {MestoService} from '@/services/MestoService'
+import { useVuelidate } from '@vuelidate/core'
+import { required, minLength, maxLength, numeric, helpers} from '@vuelidate/validators'
+
+const nameregex = helpers.regex(/^[A-Z][a-zA-Z\s]*$/);
 
 export default{
  name: "UpdateFakultet",
  components: {
   Spinner,
  },
+ setup: () => ({ v$: useVuelidate() }),
  data: function(){
    const fMestoId = this.$route.params.mestoId || '';
   return{
@@ -83,6 +91,27 @@ export default{
    errorMessage: null,
    mestoArr:[],
   }
+ },
+ validations (){
+   return{
+      fakultet: {
+        maticniBroj: {
+          required: helpers.withMessage('Polje ne moze biti prazno', required),
+          numeric: helpers.withMessage('Polje mora biti cifra', numeric),
+          maxLength: helpers.withMessage('Polje mora imati 8 cifara', maxLength(8)),
+          minLength: helpers.withMessage('Polje mora imati 8 cifara', minLength(8))
+          },
+        naziv: {
+          required: helpers.withMessage('Polje ne moze biti prazno', required),
+          nameregex: helpers.withMessage('Polje mora pocinjati velikim slovom', nameregex),
+          maxLength: helpers.withMessage('Polje mora imati maksimum 35 karaktera', maxLength(35)),
+          minLength: helpers.withMessage('Polje mora imati minimum 2 karaktera', minLength(2))
+        },
+        mestoId: {
+          required: helpers.withMessage('Polje ne moze biti prazno', required)
+        },
+      },
+   }
  },
  created: async function(){
   try {
@@ -104,15 +133,18 @@ export default{
  },
  methods: {
   fakultetUpdate: async function(){
-   try {
-    let response = await FakultetService.updateFakultet(this.fakultet, this.fakultetId);
-    if(response){
-     return this.$router.push('/fakultet');
-    }else{
-     return this.$router.push(`/fakultet/update/${this.fakultetId}`)
-    }
-   } catch (error) {
-    console.log(error)
+   this.v$.$validate();
+   if(!this.v$.$error){
+      try {
+         let response = await FakultetService.updateFakultet(this.fakultet, this.fakultetId);
+         if(response){
+            return this.$router.push('/fakultet');
+         }else{
+            return this.$router.push(`/fakultet/update/${this.fakultetId}`)
+         }
+      } catch (error) {
+         console.log(error)
+      }
    }
   }
  }
