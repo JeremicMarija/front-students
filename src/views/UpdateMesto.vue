611cee862bebdb4cbed23d0f70c53e0aaa17dcae
Marija Jeremic <marija.jeremic@pioniri.com>
2023-03-24 15:59:30 +0100

    add validation for update city

--- src/views/UpdateMesto.vue
+++ src/views/UpdateMesto.vue
@@ -34,12 +34,15 @@
     <form action="" @submit.prevent="updateMesto()">
      <div class="mb-2">
       <input v-model="mesto.ptt" type="text" class="form-control" placeholder="Ptt">
+      <span v-if="v$.mesto.ptt.$error">{{v$.mesto.ptt.$errors[0].$message}}</span>
      </div>
      <div class="mb-2">
       <input v-model="mesto.naziv" type="text" class="form-control" placeholder="Naziv">
+      <span v-if="v$.mesto.naziv.$error">{{v$.mesto.naziv.$errors[0].$message}}</span>
      </div>
      <div class="mb-2">
       <input v-model="mesto.brojStanovnika" type="text" class="form-control" placeholder="Broj stanovnika">
+      <span v-if="v$.mesto.brojStanovnika.$error">{{v$.mesto.brojStanovnika.$errors[0].$message}}</span>
      </div>
      <div class="mt-3">
       <input type="submit" class="btn btn-success text-white" value="Izmeni">
@@ -58,11 +61,16 @@
 <script>
 import {MestoService} from '@/services/MestoService';
 import Spinner from '@/components/Spinner.vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, minLength, maxLength, minValue, numeric, helpers} from '@vuelidate/validators'
+
+const nameregex = helpers.regex(/^[A-Z][a-zA-Z\s]*$/);
 export default{
  name: "UpdateMesto",
  components: {
   Spinner
  },
+ setup: () => ({ v$: useVuelidate() }),
  data: function(){
   return{
    mestoId: this.$route.params.mestoId,
@@ -73,6 +81,29 @@ export default{
     brojStanovnika: ''
    },
    errorMessage: null,
+  }
+ },
+ validations (){
+  return{
+    mesto:{
+      ptt: {
+        required: helpers.withMessage('Polje ne moze biti prazno', required),
+        numeric: helpers.withMessage('Polje mora biti cifra', numeric),
+        maxLength: helpers.withMessage('Polje mora imati 5 cifara', maxLength(5)),
+        minLength: helpers.withMessage('Polje mora imati 5 cifara', minLength(5))
+      },
+      naziv: { 
+       required: helpers.withMessage('Polje ne moze biti prazno', required),
+       nameregex: helpers.withMessage('Polje mora pocinjati velikim slovom', nameregex),
+       maxLength: helpers.withMessage('Polje mora imati maksimum 35 karaktera', maxLength(35)),
+       minLength: helpers.withMessage('Polje mora imati minimum 2 karaktera', minLength(2))
+       },
+      brojStanovnika: { 
+        required: helpers.withMessage('Polje ne moze biti prazno', required),
+        numeric: helpers.withMessage('Polje mora biti cifra', numeric),
+        minValue: helpers.withMessage('Polje mora biti vece od nule', minValue(1))
+       },
+    },
   }
  },
  created: async function(){
@@ -91,16 +122,20 @@ export default{
    return Object.keys(this.mesto).length > 0;
   },
   updateMesto: async function(){
-   try {
-    let response = await MestoService.updateMesto(this.mesto,this.mestoId);
-    if(response){
-     return this.$router.push('/mesto');
-    }else{
-     return this.$router.push(`/mesto/update/${this.mestoId}`)
-    }
-   } catch (error) {
-    console.log(error);
-   }
+     this.v$.$validate();
+     if(!this.v$.$error){
+        try {
+          let response = await MestoService.updateMesto(this.mesto,this.mestoId);
+          if(response){
+            return this.$router.push('/mesto');
+          }else{
+            return this.$router.push(`/mesto/update/${this.mestoId}`)
+          }
+        } catch (error) {
+          console.log(error);
+        }
+     }
+
   }
  }
 }
